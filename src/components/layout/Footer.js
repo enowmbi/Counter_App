9f8145ab2205d6ba49be078676b5af784afeda64
Enow Mbi <benowmbi@yahoo.com>
2019-02-27 15:40:00 +0100

    setup components and finish the footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+const Footer =() =>{
+    return(
+
+        <footer>
+            <hr/>
+        <p> &copy; 2019, Enow B. Mbi</p> </footer>
+    )
+}
+
+
+export default Footer
