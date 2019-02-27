083ca3ee60876116df0a88e934bdcb03a1c6b182
Enow Mbi <benowmbi@yahoo.com>
2019-02-27 16:05:37 +0100

    move state to counters component

--- src/components/Counters.js
+++ src/components/Counters.js
@@ -3,8 +3,17 @@ import Counter from '../components/Counter';
 
 class Counters extends Component {
 
+    state = {
+     counters: [
+         {id:1, value: 0},
+         {id:2, value: 1},
+         {id:3, value: 2}
+     ]
+    }
+
     render(){
         return(
+
             <Counter />
         
         
